b089a690d643c1d28d37bfd802915c2836b1352f
AlexVOiceover <alex@alexvoiceover.com>
2024-01-30 10:29:24 +0000

    More testing

--- src/database/db.js
+++ src/database/db.js
@@ -1,21 +1,29 @@
-const { readFileSync } = require("node:fs");
-const { join } = require("node:path");
-const Database = require("better-sqlite3");
+const { readFileSync } = require('node:fs')
+const { join } = require('node:path')
+const Database = require('better-sqlite3')
+// const { createTask } = require('../model/tasks.js')
 
-const db = new Database(process.env.DB_FILE);
+const db = new Database(process.env.DB_FILE)
 
 // const select_date = db.prepare("SELECT DATE()");
 // const result = select_date.get();
 // console.log(result);
 
-const schemaPath = join("src","database", "mySchema.sql");
-const schema = readFileSync(schemaPath, "utf-8");
-db.exec(schema);
+const schemaPath = join('src', 'database', 'mySchema.sql')
+const schema = readFileSync(schemaPath, 'utf-8')
+db.exec(schema)
 
-const select_table = db.prepare("SELECT name FROM sqlite_schema");
-const result = select_table.all();
-console.log(result);
+module.exports = db
 
-module.exports = db;
+// const select_table = db.prepare("SELECT name FROM sqlite_schema");
+// const result = select_table.all();
+// console.log(result);
 
+// const select_date = db.prepare('SELECT DATE()')
+// const result = select_date.get()
+// console.log(result)
+const { createTask } = require('../model/tasks.js')
 
+createTask('Eat a banana')
+const tasks = db.prepare('SELECT * FROM tasks').all()
+console.log(tasks)
--- src/database/mySchema.sql
+++ src/database/mySchema.sql
@@ -1,4 +1,26 @@
+PRAGMA foreign_keys = ON;
+
 BEGIN;
+
+CREATE TABLE IF NOT EXISTS categories (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS products (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  category_id INTEGER REFERENCES categories(id),
+  quantity_per_unit TEXT,
+  unit_price NUMERIC DEFAULT 0,
+  units_in_stock INTEGER DEFAULT 0,
+  units_on_order INTEGER DEFAULT 0,
+  CHECK (unit_price>=(0)),
+  CHECK (units_in_stock>=(0)),
+  CHECK (units_on_order>=(0))
+);
+
 
 CREATE TABLE IF NOT EXISTS tasks (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
